39924668102753fcf13f33887b58fd82192b2a03
Lucto <sei_zsy2015@126.com>
2017-12-21 00:20:22 +0800

    游戏大厅界面接口对接

--- static/js/hall.js
+++ static/js/hall.js
@@ -1,30 +1,40 @@
 
 window.onload = function getTables() {
-    alert("")
     $.ajax({
-        type: "POST",
-        /*url: "http://192.168.1.111:8080/trivia/session/login/",*/
-        url: "http://localhost/trivia/session/login/",
-
-        data: JSON.stringify({
-            account : $("#username").val(),
-            password : $("#userpwd").val()
-        }),
+        type: "GET",
+        url: "http://localhost/trivia/room/list/",
         contentType: "application/json; charset=utf-8",
-        dataType:"json",
+        dataType: "json",
         success: function (data) {
-            console.log(data);
             if (data.resCode === "200") {
-                console.log("succ");
-                location.href="../static/lib/lufylegend.js-lufylegend-1.10.1/examples/demo/rpg/1.html";
+                console.log(data);
+                var obj = data.data;
+                if (obj.length == 0) {
+                    alert("目前暂无房间开放，请稍后尝试！");
+                } else {
+                    var s= "<div><img src=\"../static/image/room.png\" alt=\"#\"/>\n" +
+                        "        <img class=\"t1\">\n" +
+                        "        <img class=\"t2\">\n" +
+                        "        <img class=\"t3\">\n" +
+                        "        <img class=\"t4\">\n" +
+                        "        <p id=\"id\"></p>\n" +
+                        "    </div>";
+                    var template = $(s);
+                    var sHtml;
+                    $.each(obj, function(index, item) {
+                        sHtml =template;
+                        $.each(item.playerList, function(index, item) {
+                            sHtml.find(".t"+ (index+1)+"").attr("src",item.headPic);
+                        });
+                        sHtml.find("#id").attr("value",item.roomName);
+                        $("#desks").append(sHtml);
+                    });
+                }
             }
             else {
-                alert("请确认您输入的用户名或密码输入是否正确！");
-                $("#userpwd").val("");
-                $("#userpwd").focus();
+                alert("出错啦！");
             }
         }
-
     })
-}
+};
 
