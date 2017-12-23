0e0bfb8739060508534374eb0a7faf7bd2ee32cc
Lucto <sei_zsy2015@126.com>
2017-12-23 23:45:44 +0800

    用户管理界面

--- static/js/userManage.js
+++ static/js/userManage.js
@@ -1,106 +1,72 @@
-window.onload = function userTable() {
-    var table;
-    layui.use('table', function() {
-        table = layui.table;
+$(function(){
+    loadUserTable();
+});
+
+function loadUserTable(){
+    layui.use('table', function(){
+        var table = layui.table;
         table.render({
-            //绑定容器
-            id: 'table',
             elem: '#table'
-            //设置表头
-            ,
-            cols: [
-                [
-                    { field: 'id', title: 'ID', width: 40, align: 'center', sort: 'true' }, { field: 'username', title: '用户名', width: 140, align: 'center' }, { field: 'email', title: '邮箱', width: 170, align: 'center' }, { field: 'phone', title: '手机号', width: 120, align: 'center' }, { field: 'title', title: '活动', width: 150, align: 'center' }, { field: 'status', title: '状态', width: 100, align: 'center', templet: '#statusTpl' }, { field: 'timeBegin', title: '开始时间', width: 160, align: 'center', templet: '#timeBeginTpl', sort: 'true' }, { field: 'timeEnd', title: '结束时间', width: 160, align: 'center', templet: "#timeEndTpl", sort: 'true' }, { field: 'introduction', title: '简介', align: 'center', width: 178, templet: "#introTpl" }, { field: 'gmtCreated', title: '申请时间', width: 160, align: 'center', templet: "#submitTimeTpl", sort: 'true' }, { field: 'operate', title: '操作', width: 150, fixed: 'right', align: 'center', toolbar: '#barDemo' }
-                ]
-            ],
-            page: true,
-            url: 'http://localhost/trivia/user/',
-            where: { status: $("#statu").val() },
-            method: 'get',
-            request: {
-                pageName: 'pno',
-                limitName: 'PAGE_SIZE'
-            },
-            response: {
-                statusName: 'resMsg',
+            ,height: 'full-200'
+            ,page: true //开启分页
+            ,url: 'http://localhost/trivia/user/'
+            ,method: 'get'
+            ,cols: [[ //表头
+                {field: 'id', title: 'ID', width: 80, align: 'center', sort: 'true'},
+                {field: 'account', title: '用户名', width: 150, align: 'center'},
+                {field: 'nickName', title: '昵称', width: 100, align: 'center'},
+                {field: 'headPic', title: '头像', width: 120, align: 'center', templet: '#picTpl'},
+                {field: 'userType', title: '用户类型', width: 100, align: 'center'},
+                {field: 'status', title: '状态', width: 100, align: 'center'},
+                {field: 'balance', title: '财富', width: 100, align: 'center'},
+                {field: 'lastLogin',title: '上次登陆时间',width: 180,align: 'center',templet: '#timeBeginTpl',sort: 'true'},
+                {fixed: 'right',title:'操作', width: 165, align:'center', toolbar: '#barDemo'}
+            ]]
+            ,request: {
+                pageName:"pno", //页码的参数名称，默认：page
+                limitName: "PAGE_SIZE" //每页数据量的参数名，默认：limit
+            }
+            ,response: {
+                statusName: 'resCode',
                 statusCode: '200',
-                msgName: 'errorMsg',
-                countName: 'total',
-                dataName: 'obj'
+                msgName: 'resMsg',
+                countName: 'count',
+                dataName: 'data'
             },
-            done: function(res, curr, count) { //渲染完成回调
-                initIframeHeight(600);
-            },
-            initSort: {
-                field: 'gmtCreated',
-                type: 'desc'
-            },
-            limits: [10, 20, 30],
-            limit: 10,
-            loading: true,
-            height: '500',
-            skin: 'line',
-            even: true,
             size: 'lg'
         });
+
+        //监听工具条
+        table.on('tool(table)', function(obj){ //注：tool是工具条事件名，test是table原始容器的属性 lay-filter="对应的值"
+            var data = obj.data, //获得当前行数据
+                layEvent = obj.event; //获得 lay-event 对应的值
+            if(layEvent === 'detail'){
+                layer.msg('查看操作');
+            } else if(layEvent === 'del'){
+                layer.confirm('确定删除该行吗？', function(index){
+                    //向服务端发送删除指令
+                    $.ajax({
+                        url: "http://localhost/trivia/user/"+data.id+"/",
+                        type: "DELETE",
+                        dataType: "json",
+                        success: function(data){
+                            if (data.resCode === "200") {
+                                obj.del(); //删除对应行（tr）的DOM结构
+                                layer.close(index);
+                                layer.msg('删除成功！');
+                            }else{
+                                layer.msg("删除失败！");
+                            }
+                        },
+                    });
+                });
+            } else if(layEvent === 'edit'){
+                layer.msg('编辑操作');
+            }
+        });
+
     });
+}
 
-},
 
-layui.use('table', function(){
-    var table = layui.table;
-    //监听表格复选框选择
-    table.on('checkbox(demo)', function(obj){
-        console.log(obj)
-    });
-    //监听工具条
-    table.on('tool(demo)', function(obj){
-        var data = obj.data;
-        if(obj.event === 'detail'){
-            layer.msg('ID：'+ data.id + ' 的查看操作');
-        } else if(obj.event === 'del'){
-            layer.confirm('真的删除行么', function(index){
-                obj.del();
-                layer.close(index);
-            });
-        } else if(obj.event === 'edit'){
-            layer.alert('编辑行：<br>'+ JSON.stringify(data))
-        }
-    });
 
-    var $ = layui.$, active = {
-        getCheckData: function(){ //获取选中数据
-            var checkStatus = table.checkStatus('idTest')
-                ,data = checkStatus.data;
-            layer.alert(JSON.stringify(data));
-        }
-        ,getCheckLength: function(){ //获取选中数目
-            var checkStatus = table.checkStatus('idTest')
-                ,data = checkStatus.data;
-            layer.msg('选中了：'+ data.length + ' 个');
-        }
-        ,isAll: function(){ //验证是否全选
-            var checkStatus = table.checkStatus('idTest');
-            layer.msg(checkStatus.isAll ? '全选': '未全选')
-        }
-    };
-
-    $('.demoTable .layui-btn').on('click', function(){
-        var type = $(this).data('type');
-        active[type] ? active[type].call(this) : '';
-    });
-});
-
-layui.use('element', function(){
-    var element = layui.element; //导航的hover效果、二级菜单等功能，需要依赖element模块
-
-    //监听导航点击
-    element.on('nav(demo)', function(elem){
-        //console.log(elem)
-        layer.msg(elem.text());
-    });
-});
-
-layui.use('table', function(){
-    var table = layui.table;
-});
