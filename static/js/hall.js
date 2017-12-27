ef61965997bb3ca851e1fc1eef500e46e6376182
Lucto <sei_zsy2015@126.com>
2017-12-27 23:53:53 +0800

    大厅webSocket

--- static/js/hall.js
+++ static/js/hall.js
@@ -14,6 +14,14 @@ window.onload = function getTables() {
     };
     websocket.onmessage = function(evnt) {
         $('#msg').html($("#msg").html() + "<br/>" + evnt.data);
+        switch(data.type){
+            case 0:
+                refreshPlayerList();
+                break;
+            case 1:
+                getMessage(data);
+                break;
+        }
     };
     websocket.onerror = function(evnt) {};
     websocket.onclose = function(evnt) {
@@ -106,4 +114,88 @@ function enterRoom(roomId){
         }
     });
 
+}
+
+function refreshPlayerList(){
+    $("#desks").clear();
+    $.ajax({
+        type: "GET",
+        url: "http://localhost/trivia/room/list/",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            if (data.resCode === "200") {
+                console.log(data);
+                var obj = data.data;
+                if (obj.length == 0) {
+                    alert("目前暂无房间开放，请稍后尝试！");
+                } else {
+                    var s="<div><div class=\"desk\"><img class=\"pic\" src=\"../static/image/room.png\"  alt=\"#\"/>\n" +
+                        "        <img class=\"t1_\">\n" +
+                        "        <img class=\"t2_\">\n" +
+                        "        <img class=\"t3_\">\n" +
+                        "        <img class=\"t4_\">\n" +
+                        "        <span id=\"id\"></span>\n" +
+                        "    </div></div>";
+                    var sHtml;
+                    $.each(obj, function(index, item) {
+                        sHtml = $(s);
+                        sHtml.find(".pic").attr("onclick","enterRoom("+item.id+")");
+                        $.each(item.playerList, function(index, item) {
+                            sHtml.find(".t"+ (index+1)+"_").attr("class","t"+(index+1)+"");
+                            sHtml.find(".t"+ (index+1)+"").attr("src",item.headPic);
+                        });
+                        sHtml.find("#id").html("- " +item.roomName+" -");
+                        $("#desks").append(sHtml.html());
+                    });
+                }
+            }
+            else {
+                alert("出错啦！");
+            }
+        }
+    });
+}
+
+function getMessage(data){
+    var chat = "<div>"+data.user.nickName+" "+data.time+"\n"+"data.content</div>";
+    $("#chatinf").append(chat.html());
+}
+
+function sendMessage(){
+
+    var myDate = new Date();
+//获取当前年
+    var year=myDate.getFullYear();
+//获取当前月
+    var month=myDate.getMonth()+1;
+//获取当前日
+    var date=myDate.getDate();
+    var h=myDate.getHours();       //获取当前小时数(0-23)
+    var m=myDate.getMinutes();     //获取当前分钟数(0-59)
+    var s=myDate.getSeconds();
+
+    var now = year+'-'+p(month)+"-"+p(date)+" "+p(h)+':'+p(m)+":"+p(s);
+    var message = $("#input").val();
+    $.ajax({
+        url: "http://localhost/trivia/message/hall/all/",
+        type: "GET",
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        data:{
+            message:message
+        },
+        success: function(data){
+            if (data.resCode === "200") {
+                var chat = "<div>我 "+ now + "\n" + message + "</div>";
+                $("#chatinf").append(chat.html());
+            }else{
+                alert("您发不了消息！");
+            }
+        }
+    });
+}
+
+function p(s) {
+    return s < 10 ? '0' + s: s;
 }
